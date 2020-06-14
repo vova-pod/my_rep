cfd1daed437a1a0d81933c694d2cb13597860151
vova-pod <vladimir.podrezov@me.com>
2020-06-14 11:46:12 +0100

    Remove sensitive data from setting.py

--- web_travel_balance/web_travel_balance/settings.py
+++ web_travel_balance/web_travel_balance/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rz8k&bq@f!vsg^f@g6h@xf4&g9x5#mi_pnvm_e9f1gh*gv!ivm'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -33,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     'web_travel_data',
+    'users',
     # Default Django apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -125,9 +127,8 @@ STATIC_URL = '/static/'
 
 # Email settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "cutterw7@gmail.com"
-EMAIL_HOST_PASSWORD = "AFZM8402goog"
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "cutterw7@gmail.com"
